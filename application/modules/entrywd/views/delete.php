14e4a8384a9e6643080353363ad5c20e9002fd96
imufiid <imufiid98@gmail.com>
2020-01-28 20:17:32 +0700

    deleted

--- application/modules/entrywd/views/delete.php
+++
@@ -1,6 +0,0 @@
-<div class="row">
-  <div class="col-md-12">
-    <input type="hidden" name="id" value="<?= $condition->id ?>">
-    <p>Anda yakin menghapus data <b><?=$condition->condition?></b>?</p>
-  </div>
-</div>
